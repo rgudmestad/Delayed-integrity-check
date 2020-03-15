bae00844aa3de22105ce2c65ea81aa0bf8682d43
rgudmestad <52523429+rgudmestad@users.noreply.github.com>
2020-03-15 18:42:12 +0100

    Update PMU.py
    works with UDP and real data!

--- PMU.py
+++ PMU.py
@@ -1,43 +1,29 @@
 import socket
-from time import sleep
+import pandas as pd
+import time
 
-host = socket.gethostname()                           
-hmac_pc_port = 8000                        
-controll_pc_port = 9000
-#x = True
-messages = []
+host = socket.gethostname()                   
+hmac_address =  ("127.0.0.1", 8001)                     
+controll_address =  ("127.0.0.1", 9001)
+pmu= socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+df = pd.read_csv("wiresharkcapture.csv")                                    # Wireshark capture of IEV 61850 SM traffic, converted into a CVS format
+length = df.shape[0]                                                        # length = number of captured packets
 
+for i in range(0, length):
+    y = df.iloc[i]                                                          # Formats the CSV file so that it only sends the captured packet, and notthing else 
+    y = y.to_string(header=None).split("\n")                                # --"--
+    y = [','.join(ele.split()) for ele in y]                                # --"--
+    y = "".join(y)                                                          # --"--
+    y = str.encode(y)                                                       # --"--
+  
+    pmu.sendto(y, hmac_address)                                             # Sends a packet to hmac pc
+    pmu.sendto(y, controll_address)                                         # Sends a packet to controll pc
 
-
-
-
-# PRØV UDP!!!! socket.SOCK_DGRAM 
-
-# PMU.py sends a message to both hmac_pc and controll_pc each 0.015 second. 
-# Each message consists of:  {"id + "test"} e.g. {1 "test"}, {2 "test"} etc...
-for i in range(0, 3000):
-    value = str(i) + " test"
-
-    pmu_to_hmac_pc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pmu_to_hmac_pc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #pmu_to_hmac_pc.bind((host, 6800))
-    pmu_to_hmac_pc.connect((host, hmac_pc_port))
-    pmu_to_hmac_pc.send(value.encode("utf-8"))
-    pmu_to_hmac_pc.shutdown(socket.SHUT_RDWR)
-    pmu_to_hmac_pc.close()  
-
-
-    pmu_to_controll_pc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pmu_to_controll_pc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #pmu_to_controll_pc.bind((host, 6700)) 
-    pmu_to_controll_pc.connect((host, controll_pc_port))
-    pmu_to_controll_pc.send(value.encode("utf-8"))
-    pmu_to_controll_pc.shutdown(socket.SHUT_RDWR)
-    pmu_to_controll_pc.close()
-
-    if i% 250 == 0:
-        print(i, " GOOSE messages sent to both HMAC and controll PC")
-    sleep(0.015) # max rate of PMU GOOSE messages
+    if i% 1000 == 0:
+        print(i, " GOOSE messages sent to both HMAC and controll PC")       # Print to keep track of nr of packets sent
+    time.sleep(0.01)                                                        # max rate of PMU GOOSE messages       
     
-print("finito!")
+print("finitoo!")
+
+
