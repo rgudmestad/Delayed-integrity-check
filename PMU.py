4426ab3b7b4d8551628f0dd71437f40861f51057
rgudmestad <52523429+rgudmestad@users.noreply.github.com>
2020-04-16 13:39:32 +0200

    Update PMU.py

--- PMU.py
+++ PMU.py
@@ -2,28 +2,30 @@ import socket
 import pandas as pd
 import time
 
-host = socket.gethostname()                   
-hmac_address =  ("127.0.0.1", 8001)                     
-controll_address =  ("127.0.0.1", 9001)
-pmu= socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+host = socket.gethostname()                                                 # Gets the IP-address of the PC            
+hmac_address =  ("127.0.0.1", 8001)                                         # IP address and port of HMAC-PC
+controll_address =  ("127.0.0.1", 9001)                                     # Ip address and port of Control-pc
+pmu= socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)           # Creates an UDP socket to send from
 
-df = pd.read_csv("wiresharkcapture.csv")                                    # Wireshark capture of IEV 61850 SM traffic, converted into a CVS format
-length = df.shape[0]                                                        # length = number of captured packets
+
+with open('SVpackets.txt') as f:
+    mylist = list(f)
+GOOSE_Array =[]
+for j in range(0, len(mylist)):
+    if j % 18 == 0:
+        packet = ""
+        for i in range(j, j+17):
+            packet = packet + mylist[i]
+        GOOSE_Array.append(packet)
+length = len(GOOSE_Array)
 
 for i in range(0, length):
-    y = df.iloc[i]                                                          # Formats the CSV file so that it only sends the captured packet, and notthing else 
-    y = y.to_string(header=None).split("\n")                                # --"--
-    y = [','.join(ele.split()) for ele in y]                                # --"--
-    y = "".join(y)                                                          # --"--
-    y = str.encode(y)                                                       # --"--
-  
-    pmu.sendto(y, hmac_address)                                             # Sends a packet to hmac pc
-    pmu.sendto(y, controll_address)                                         # Sends a packet to controll pc
-
+    pmu.sendto(str.encode(GOOSE_Array[i]), hmac_address)                    # Sends a packet to hmac pc
+    pmu.sendto(str.encode(GOOSE_Array[i]), controll_address)                # Sends a packet to controll pc
     if i% 1000 == 0:
         print(i, " GOOSE messages sent to both HMAC and controll PC")       # Print to keep track of nr of packets sent
     time.sleep(0.01)                                                        # max rate of PMU GOOSE messages       
     
-print("finitoo!")
+print("Program finished")
 
 
