22c520f5d78c0e769faacc23f283e1ad6d9f2bfc
rgudmestad <52523429+rgudmestad@users.noreply.github.com>
2020-06-08 10:56:19 +0200

    Update PMU.py

--- PMU.py
+++ PMU.py
@@ -3,8 +3,8 @@ import pandas as pd
 import time
 from random import randrange
 import random
-
-
+import hashlib
+import hmac
 host = socket.gethostname()                                                 # Gets the IP-address of the PC            
 hmac_address =  ("127.0.0.1", 8001)                                         # IP address and port of HMAC-PC
 controll_address =  ("127.0.0.1", 9001)                                     # Ip address and port of Control-pc
@@ -22,13 +22,8 @@ for j in range(0, len(mylist)):                                             # It
 length = len(GOOSE_Array)
 
 for i in range(0, length):                                                  # For each GOOSE msg DO:
-    #random.seed(i)
-    #randval = random.randrange(0,100) 
-    if i > 1250 and i < 1500:
-        pmu.sendto(str.encode(GOOSE_Array[i]), controll_address)            # Sends a packet to controll pc
-    else:
-        pmu.sendto(str.encode(GOOSE_Array[i]), hmac_address)                # Sends a packet to hmac pc
-        pmu.sendto(str.encode(GOOSE_Array[i]), controll_address)            # Sends a packet to controll pc
+    pmu.sendto(str.encode(GOOSE_Array[i]), hmac_address)                    # Sends a packet to hmac pc
+    pmu.sendto(str.encode(GOOSE_Array[i]), controll_address)                # Sends a packet to controll pc
     if i% 250 == 0:
         print("\n", i, "GOOSE messages sent to both HMAC and controll PC")  # Print to keep track of nr of packets sent
     time.sleep(0.01)                                                        # max rate of PMU GOOSE messages           
